d015e6437e0b53c250e85c24bdc2c0e2c777f598
nav <navid.pirsajed@gmail.com>
2024-05-03 19:20:43 -0300

    chore: Update setup.sh script for user creation and permissions

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # script to get a linux system ready for development
 
 echo "starting" &&
@@ -44,23 +46,34 @@ echo "starting" &&
         npm \
         python3 \
         python3-pip \
-        ssh-keygen \
+        zsh \
+        ufw \
         build-essential &&
 
-    # install oh-my-zsh
-    echo "installing oh-my-zsh" &&
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
+    # change default shell to zsh
+    chsh -s $(which zsh) &&
+    echo "dependencies installed" &&
+    echo "setting up root" &&
+    echo "root:$root_password" | chpasswd &&
+    echo "root setup complete" &&
 
     # create a new user
-    echo "creating a new user" &&
-    useradd -m -s /bin/zsh $username &&
-    echo "$username:$password" | chpasswd &&
+    if id "$username" &>/dev/null; then
+        echo "user exists"
+    else
+        echo "creating a new user" &&
+            useradd -m -s /bin/zsh $username &&
+            echo "$username:$password" | chpasswd &&
+            echo "user created" &&
+            # fix permissions
+            echo "fixing permissions" &&
+            chown -R $username:$username /home/$username &&
+            echo "permissions fixed" &&
 
-    # add user to sudo and docker group
-    echo "adding $username to sudo group" &&
-    usermod -aG sudo $username &&
-    echo "adding $username to docker group" &&
-    usermod -aG docker $username &&
+            # add user to sudo
+            echo "adding $username to sudo group" &&
+            usermod -aG sudo $username
+    fi &&
 
     # check if docker is installed
     echo -e "\e[32mChecking if docker is installed" &&
@@ -73,8 +86,7 @@ echo "starting" &&
             curl -fsSL https://get.docker.com -o get-docker.sh &&
             echo "running docker install script" &&
             sh get-docker.sh &&
-            rm get-docker.sh &&
-            sudo usermod -aG docker $USER
+            rm get-docker.sh
     fi &&
 
     # install docker-compose
@@ -84,7 +96,30 @@ echo "starting" &&
     # set up ssh
     echo "setting up ssh" &&
     echo "generating ssh key" &&
-    # gemerate ssh key for the new user and root
-    sudo -u $username ssh-keygen -t rsa -b 4096 -C "$username@$(hostname)" -f /home/$username/.ssh/id_rsa -N "" &&
-    ssh-keygen -t rsa -b 4096 -C "root@$(hostname)" -f /root/.ssh/id_rsa -N "" &&
-    EOF
+
+    # set up git
+    echo "setting up git" &&
+    git config --global user.name "$git_name" &&
+    git config --global user.email "$git_email" &&
+    echo "git setup complete" &&
+
+    #  ssh key for the new user and root
+    sudo -u $username ssh-keygen -t rsa -b 4096 -C "$username@$(hostname)" -f /home/$username/.ssh/id_rsa -N "$ssh_passphrase" &&
+    ssh-keygen -t rsa -b 4096 -C "root@$(hostname)" -f /root/.ssh/id_rsa -N "$ssh_passphrase" &&
+
+    # set up ssh config
+    sed -i "s/#Port 22/Port $ssh_port/g" /etc/ssh/sshd_config &&
+    sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin no/g" /etc/ssh/sshd_config &&
+    sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config &&
+    sed -i "s/#PubkeyAuthentication yes/PubkeyAuthentication yes/g" /etc/ssh/sshd_config &&
+    systemctl restart sshd &&
+    echo "ssh setup complete" &&
+
+    # display info
+    echo "Server port: $ssh_port" &&
+    echo "Server public key: /root/.ssh/id_rsa.pub" &&
+    echo "
+    User: $username
+    User public key: $(/home/$username/.ssh/id_rsa.pub)
+    Root public key: $(/root/.ssh/id_rsa.pub)
+    "
